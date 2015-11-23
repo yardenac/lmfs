51fb4a60fc5a71762b33b4772b7ff5eca20ab8bd
yar <yardenack@gmail.com>
2015-11-22 18:35:36 -0800

    skeleton

---
+++ lmfs.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+from fuse import FUSE, FuseOSError, Operations
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+setup(name='lmfs',
+      version='0.1',
+      description='Least Mansplaining Filesystem',
+      license='GPL3',
+      author='yar',
+      email='yardenack@gmail.com',
+      url='https://github.com/yardenac/lmfs',
+      py_modules=['lmfs','fuse'],
+      requires=[],
+)
